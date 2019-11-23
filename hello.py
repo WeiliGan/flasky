e316ff9d3d40ba1637e1afe18c3ac1f7f6cb237d
WeiliGan <gwl20190610@163.com>
2019-11-23 21:58:19 +0800

    响应示例1

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask import request
+from flask import make_response
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browers is {}</p>'.format(user_agent)
+    response = make_response('<h1>This is document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
