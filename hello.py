2ca8735648e0f2a1af21bd921fb64a80e88fe759
WeiliGan <gwl20190610@163.com>
2019-11-23 22:14:43 +0800

    重定向

--- hello.py
+++ hello.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask import request
 from flask import make_response
+from flask import redirect
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def index():
+#     response = make_response('<h1>This is document carries a cookie!</h1>')
+#     response.set_cookie('answer', '42')
+#     return response
+
 @app.route('/')
 def index():
-    response = make_response('<h1>This is document carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
-    return response
-
+    return redirect('http://www.example.com')
