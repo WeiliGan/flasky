8f5d58b563c75babbb859f9c912362e7be1447c6
WeiliGan <gwl20190610@163.com>
2019-11-25 00:33:49 +0800

    重定向和用户会话

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -17,25 +17,8 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/user/<name>')
-# def user(name):
-#     return render_template('user.html', name=name)
-
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return render_template('404.html'), 404
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html', current_time=datetime.utcnow())
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
