3a7d90e79505bf40177cdbf210c698c483d1b9ae
WeiliGan <gwl20190610@163.com>
2019-11-24 20:56:09 +0800

    使用Flask-Moment本地化日期和时间

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstarp = Bootstrap(app)
+moment = Moment(app)
 
 # @app.route('/')
 # def index():
@@ -12,10 +15,10 @@ bootstarp = Bootstrap(app)
 def user(name):
     return render_template('user.html', name=name)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('404.html'), 404
 
-# @app.errorhandler(500)
-# def internal_server_error(e):
-#     return render_template('500.html'), 500
+@app.route('/')
+def index():
+    return render_template('index.html', current_time=datetime.utcnow())
