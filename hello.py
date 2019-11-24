fd3189963862b4fcdc5827e8b0820da4eff1e260
WeiliGan <gwl20190610@163.com>
2019-11-24 23:49:00 +0800

    配置、表单类、把表单渲染成HTML在视图函数中处理表单

--- hello.py
+++ hello.py
@@ -2,23 +2,40 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstarp = Bootstrap(app)
 moment = Moment(app)
+
+class NameForm(FlaskForm):
+    name = StringField('what is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
 
 # @app.route('/')
 # def index():
 #     return render_template('index.html')
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+# @app.route('/user/<name>')
+# def user(name):
+#     return render_template('user.html', name=name)
 
 # @app.errorhandler(404)
 # def page_not_found(e):
 #     return render_template('404.html'), 404
 
-@app.route('/')
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+# @app.route('/')
+# def index():
+#     return render_template('index.html', current_time=datetime.utcnow())
