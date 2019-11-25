515b354920d3f1202c171c174980ade5c8666d2a
WeiliGan <gwl20190610@163.com>
2019-11-26 00:53:30 +0800

    集成Python shell

--- hello.py
+++ hello.py
@@ -59,3 +59,7 @@ def index():
         form.name.data = ''
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
